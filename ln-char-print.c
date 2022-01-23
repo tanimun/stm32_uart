2b2ffed7989af907cde1425bd040c7f6ca291b79
tanimun <91817718+tanimun@users.noreply.github.com>
2022-01-23 16:01:27 +0600

    Create ln-char-print.c

---
+++ ln-char-print.c
@@ -0,0 +1,59 @@
+#include "stm32f10x.h"                  // Device header
+
+void USART2_write (int ch);
+void delayMs(int delay);
+
+int main(void)
+{
+
+	char a[] = {"Salamun Alykum"};
+	char b[] = {"Walaykum Salam"};
+
+	
+	RCC->APB2ENR |=0x0004;
+	RCC->APB1ENR |=0X20000;
+	
+	GPIOA ->CRL = 0x00004A00;
+	
+	USART2->CR1 |= 0x2000;
+	USART2->CR1 |= 0x0008;
+	
+	USART2->BRR = 0x0EA6;
+	
+	while(1)
+	{
+	for(int i=0; a[i]!=0; i++)
+		{
+			USART2_write(a[i]);
+		}
+	delayMs(50);
+	USART2_write('\r');
+		delayMs(50);
+		USART2_write('\n');
+		delayMs(50);
+		
+		for (int i = 0; b[i]!=0; i++)
+		{
+			USART2_write(b[i]);
+		}
+		delayMs(50);
+		USART2_write('\r');
+		delayMs(50);
+		USART2_write('\n');
+		
+	}
+}
+	
+	void USART2_write (int ch)
+	{
+while(!(USART2 ->SR & 0x0080)){}
+		USART2 ->DR = (ch & 0xFF);
+}
+	void delayMs(int delay)
+	{
+	int i;
+	for(; delay>0;delay--)
+	{
+		for(i = 0; i<3195; i++);
+	}
+}
